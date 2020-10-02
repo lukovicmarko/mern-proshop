288163f4d88d52be175333442b40ac1c1e146556
marko lukovic <lukovic86@gmail.com>
2020-10-02 21:18:38 +0200

    added backend

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, Card, ListGroup, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import axios from 'axios';
+
+const ProductScreen = ({ match }) => {
+    const [product, setProduct] = useState({});
+
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+            setProduct(data);
+        }
+
+        fetchProduct();
+    }, []);
+
+    const { name, image, rating, numReviews, price, description, countInStock } = product;
+
+    return (
+        <>
+            <Link className="btn btn-light my-3" to="/">
+                Go Back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={image} alt={name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h3>{name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating
+                                value={rating}
+                                text={numReviews}
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price: ${price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: ${description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Price:
+                                    </Col>
+                                    <Col>
+                                        <strong>
+                                            {price}
+                                        </strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Status:
+                                    </Col>
+                                    <Col>
+                                        {countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button
+                                    className="btn-block"
+                                    type="button"
+                                    disabled={countInStock === 0}
+                                >
+                                    Add To Cart
+                            </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+
+        </>
+    )
+}
+
+export default ProductScreen
